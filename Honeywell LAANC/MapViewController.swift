0c70d56aed1e9965176285530e09fc5b2bbbd7f8
Harsha <harsha.muddu@gmail.com>
2019-02-17 00:36:35 -0500

    Crude Polygon drawing working

--- Honeywell LAANC/MapViewController.swift
+++ Honeywell LAANC/MapViewController.swift
@@ -9,13 +9,64 @@
 import UIKit
 import GoogleMaps
 
-class MapViewController: UIViewController {
+
+
+class MapViewController: UIViewController, GMSMapViewDelegate {
+    
+    //var mapView: GMSMapView!
+    var coordinates = [CLLocationCoordinate2D]()
+    var mapView:GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //    func mapView(_ mapView: GMSMapView, didBeginDragging marker: GMSMarker) {
+    //        print(marker.position)
+    //        print("HELLO")
+    //    }
     
     override func loadView() {
-        let initial_lat = 33.774002
-        let initial_long = -84.384122
-        let camera = GMSCameraPosition.camera(withLatitude: initial_lat, longitude: initial_long, zoom: 10.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.settings.scrollGestures = false
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        marker.isDraggable = true
+        
+        mapView.delegate = self
         view = mapView
     }
+    
+    func mapView(_ mapView: GMSMapView, didBeginDragging marker: GMSMarker) {
+        coordinates.append(marker.position)
+    }
+    
+    func mapView(_ mapView: GMSMapView, didDrag marker: GMSMarker) {
+        coordinates.append(marker.position)
+    }
+    
+    func mapView(_ mapView: GMSMapView, didEndDragging marker: GMSMarker) {
+        coordinates.append(marker.position)
+        coordinates.append(coordinates.first!)
+        createPolygon()
+    }
+    
+    func createPolygon() {
+        let polypath = GMSMutablePath()
+        coordinates.forEach { loc in
+            polypath.add(loc)
+        }
+        let polygon = GMSPolygon(path: polypath)
+        polygon.map = mapView
+    }
+    
+    
+    
 }
+
